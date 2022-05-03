193b3a3c5e68e184112b3a107ff972852e4fca24
Lakmal Weerasekara <lakmal.weerasekara@nationstrust.com>
2022-05-03 07:27:58 +0530

    added search edit field

---
+++ lib/utils/global_widgets/search_edit_field.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/utils/styles/resources_constant.dart';
+import 'package:news_app/utils/styles/theme_extension.dart';
+class SearchEditField extends StatelessWidget {
+  const SearchEditField({
+    Key? key,
+    this.onChanged,
+    this.onTap
+  }) : super(key: key);
+
+  final Function(String)? onChanged;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        height: 42,
+        child: TextField(
+          onChanged: onChanged,
+          onTap: onTap,
+          maxLines: 1,
+          textAlign: TextAlign.left,
+          style: context.semibold12pxTextStyle(context.focusedTextColor),
+          decoration: InputDecoration(
+            contentPadding:
+                const EdgeInsets.symmetric( horizontal: 16),
+            suffixIcon: Image.asset(icSearch),
+            hintText: hintText,
+            hintStyle: context.semibold12pxTextStyle(context.greyHintTextColor),
+            focusedBorder: foucuedBorder(context),
+            enabledBorder: enabledBorder(context),
+            border: enabledBorder(context),
+          ),
+        ),
+      ),
+    );
+  }
+
+  enabledBorder(BuildContext context)=>OutlineInputBorder(
+                borderSide: BorderSide(color:  context.greyHintTextColor.withOpacity(0.2)),
+                borderRadius: BorderRadius.circular(25.0));
+
+  foucuedBorder(BuildContext context)=>OutlineInputBorder(
+                borderSide: BorderSide(color:  context.greyHintTextColor.withOpacity(0.5)),
+                borderRadius: BorderRadius.circular(25.0));
+}
