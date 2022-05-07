1ec40645fa6b564eb8c0d7842afdd55d02f60f76
Lakmal Weerasekara <lakmal.weerasekara@nationstrust.com>
2022-05-08 00:02:31 +0530

    cleanup

--- lib/modules/dashboard/dashboard_controller.dart
+++ lib/modules/dashboard/dashboard_controller.dart
@@ -1,13 +1,6 @@
-import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
-import 'package:news_app/data/models/articles_model.dart';
-import 'package:news_app/data/repository/news_repository.dart';
-import 'package:news_app/services/loading_progress_service.dart';
 import 'package:news_app/utils/enum.dart';
 import 'package:news_app/utils/extension.dart';
-import 'package:news_app/utils/failure.dart';
-
-import '../../data/remote/response/news_list_response.dart';
 
 class DashboardController extends GetxController {
   final selectedIndex = eBottomNavigation.home.tabIndex.obs;
