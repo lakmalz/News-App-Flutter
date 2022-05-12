c94cd2508919960bebf70593c1b388449d47a3b9
Lakmal Weerasekara <lakmal.weerasekara@nationstrust.com>
2022-05-12 07:55:45 +0530

    alternative API keys

--- lib/data/remote/api_constant.dart
+++ lib/data/remote/api_constant.dart
@@ -4,11 +4,8 @@ class APIConstant {
   static const defaultTimeout = Duration.millisecondsPerMinute;
   static const contentType = 'application/json';
   static const xAPIKey = 'X-Api-Key';
-  static const apiKey = 
-  // '1f681d4633ee4826a13339a716a8378e';
-  // TODO remove this lines
+  static const apiKey = '9284c0a465834e018c24e134ac255a72';
   // Alternative keys for testing 
   // '9284c0a465834e018c24e134ac255a72';
-  'f0383482278d4b14a0d981d345ce9661'; 
-
+  // '1f681d4633ee4826a13339a716a8378e';
 }
